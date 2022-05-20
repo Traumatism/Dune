a6c78092710c9e28fee852adebf5bee692f5eb52
toast <cpastoast@protonmail.com>
2022-05-20 18:01:10 +0200

    update

--- dune/__init__.py
+++
@@ -1,8 +0,0 @@
-from rich.console import Console
-
-console = Console()
-
-version = "1.0.0"
-author = "@toastakerman"
-
-MODULES = []
--- dune/__main__.py
+++ dune/__main__.py
@@ -1,94 +1,31 @@
-import argparse
-import requests
-import sys
+import rich
+import typing
+
+from .table import Table
+from .database import Database
 
 
-from typing import Iterator, Type
-from urllib3 import disable_warnings
+class User(typing.NamedTuple):
+    """User object"""
 
-from dune import console, version, author
-from dune.common.base import DuneModule
-
-from dune.modules.public_snippets import PublicSnippets
-from dune.modules.user_enum import UserEnum
-
-MODULES = {PublicSnippets, UserEnum}
+    name: str
+    age: int
 
 
-def get_modules() -> Iterator[Type[DuneModule]]:
-    """ Get modules """
-    for module in MODULES:
-        if issubclass(module, DuneModule):
-            yield module
+db = Database()
 
+users: Table[User] = Table("users")
 
-if __name__ == "__main__":
-    disable_warnings()
+users.insert(User("John", 25))
+users.insert(User("Jane", 24))
+users.insert(User("Jack", 23))
 
-    console.print(
-        r"""[yellow]
-[orange3]/\_/\ [/orange3] [bold]Dune v%(version)s[/bold]
-[orange3]\, ,/[/orange3]  [dim]A Gitlab pwning tool[/dim]
-[orange3] \_/ [/orange3]  [red]Author: %(author)s[/red]
-[/yellow]
-        """ % {"version": version, "author": author}
-    )
+db.add_table(users)
 
-    parser = argparse.ArgumentParser()
+rich.print(list(users.get_func(lambda user: user.age >= 23)))
 
-    target_specification = parser.add_argument_group(
-        "TARGET SPECIFICATION", description="Define the target(s) to scan"
-    )
+rich.print(list(users.get_func(lambda user: user.age >= 23, limit=1)))
 
-    target_specification.add_argument(
-        "-t", "--target",
-        type=str,
-        required=False,
-        help="Target to scan",
-        metavar="<url>",
-        dest="target",
-        default=""
-    )
+rich.print(db.export())
 
-    target_specification.add_argument(
-        "-l", "--list",
-        type=str,
-        required=False,
-        help="Path to file with URLs",
-        metavar="<path>",
-        dest="path",
-        default=""
-    )
-
-    arguments = parser.parse_args()
-
-    if (
-        tuple(map(bool, (arguments.target, arguments.path)))
-        in ((True, True), (False, False))
-    ):
-        console.log(
-            "You must specify a single target "
-            "[underline]or[/underline] a file path."
-        )
-
-        sys.exit(0)
-
-    if arguments.path:
-        console.log("Not implemented yet.")
-        sys.exit(0)
-
-    console.log(f"Connecting to '{arguments.target}'")
-
-    try:
-        response = requests.get(arguments.target, verify=False)
-    except requests.exceptions.RequestException:
-        console.log("Can't connect to target.")
-        sys.exit(0)
-
-    console.log("Target is up and running.")
-
-    for module in get_modules():
-        console.log(f"Running module '{module.name}' {module.description}")
-        module(arguments.target).run()
-
-    console.log("Done!")
+rich.print(db.to_bin())
